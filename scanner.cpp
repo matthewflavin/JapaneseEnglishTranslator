1b305976db48b8b0b06f1dfb0584c3484b9e70b9
Matthew <matthewflavin123@gmail.com>
2020-10-28 16:57:49 -0700

    Update scanner.cpp
    Updated to his example file.

--- scanner.cpp
+++ scanner.cpp
@@ -1,79 +1,29 @@
-// CS 421 Project Part A - The Scanner
-// Group Members: Matthew Flavin, Jesse Cox, Jackson Koshley
-// Members A and C: Jesse Cox and Jackson Koshley
-// Member B: Matthew Flavin
-
-// Include libraries
-#include <iostream>
-#include <string>
-
-// Namespaces
+#include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
-// Function Prototypes
-string IsWordType(string word);
-string IsReserved(string word);
-bool IsPeriod(string word);
 bool IsVowel(char token);
 bool IsConstantPairStarter(char token);
 bool IsConstantPairStarterVariant(char token);
 bool IsConstantNotPairStarter(char token);
 
-// MAIN()
-// DRIVER CODE GOES HERE
-int main()
-{
-    // Matthew -
-    // This is the code I was using to test the functions.
-    // It does not match his requirements, I don't think.
-    // Need proper driver code. 
+/* Look for all **'s and complete them */
 
-    string testInput = "";
-    string wordType = "";
+//=====================================================
+// File scanner.cpp written by: Group Number: ** 
+//=====================================================
 
-    cout << "DRIVER CODE TEST, ENTER stop TO STOP PROGRAM.";
+// --------- Two DFAs ---------------------------------
 
-    while (testInput != "stop")
-    {
-        cout << endl << "ENTER WORD: ";
-        cin >> testInput;
-        cout << endl;
-
-        if (testInput == "stop")
-        {
-            break;
-        }
-
-        else
-        {
-            wordType = IsReserved(testInput);
-
-            if (wordType == "NONE")
-            {
-                if (IsPeriod(testInput))
-                {
-                    wordType = "PERIOD";
-                }
-
-                else
-                {
-                    wordType = IsWordType(testInput);
-                }     
-            }
-
-            cout << testInput << " : " << wordType << endl;
-        }
-    }
-
-    return 0;
-}
-
-// inputs: a string.
-// outputs: 3 possible strings, "WORD1", "WORD2", "LEXICAL ERROR".
+// WORD DFA 
+// Done by: * Matthew Flavin *
+// RE:   * (vowel | vowel n | consonant vowel | consonant vowel n | consonant-pair vowel | consonant-pair vowel n)^+ *
+// outputs: bools
 // This function is modeled after the DFA of the NFA given.
 // It iterates throughout a word, following states based on the given character.
 // An enum named wordType is created to keep track of the current states.
-string IsWordType(string word)
+bool word (string word)
 {
     // Check that string exists and is not empty.
     if (word.size() > 0)
@@ -264,96 +214,114 @@ string IsWordType(string word)
         // Check that the loop ended in a final state. (q0, q0q1, q0qy)
         if (currentState == q0 || currentState == q0q1 || currentState == q0qy)
         {
-            // Get the last character of the input string and store it in finalChar.
-            char finalChar = word[word.size() - 1];
-
-            // If final character of string is capital I or E, it is type WORD2.
-            if (finalChar == 'I' || finalChar == 'E')
-                return "WORD2";
-
-            // Else, it is type WORD1
-            else
-                return "WORD1";
+            return true;
         }
 
         // Return Error Statement.
         else
         {
-            return "LEXICAL ERROR";
+            return false;
         }
     }
 }
 
-// inputs: a string.
-// outputs: a bool value.
-// This function checks whether the given string consists of only a period character. If it does, return true.
-bool IsPeriod(string word)
+// PERIOD DFA 
+// Done by: * Matthew Flavin *
+bool period (string word)
 {
     // Check that word size is only 1.
-    if (word.size() == 1)
-    {
-        // Check if the only token in the word is a period, if so, return true.
-        if(word[0] == '.')
-        {
-            return true;
-        }
-    }
+  if (word.size() == 1)
+  {
+      // Check if the only token in the word is a period, if so, return true.
+      if(word[0] == '.')
+      {
+          return true;
+      }
+  }
 
-    // Not a period, return false.
-    return false;
+  // Not a period, return false.
+  return false;
 }
 
-// inputs: a string.
-// outputs: a string.
-// This function checks the input word against the reserved words of the language.
-// If a match is found, the function returns the matching type as a string.
-// If no match is found, "NONE" is returned.
-string IsReserved(string word)
+// ------ Three  Tables -------------------------------------
+
+// TABLES Done by: **
+
+// ** Update the tokentype to be WORD1, WORD2, PERIOD, ERROR, EOFM, etc.
+enum tokentype {ERROR, };
+
+// ** For the display names of tokens - must be in the same order as the tokentype.
+string tokenName[30] = { }; 
+
+// ** Need the reservedwords table to be set up here. 
+// ** Do not require any file input for this. Hard code the table.
+// ** a.out should work without any additional files.
+
+
+// ------------ Scanner and Driver ----------------------- 
+
+ifstream fin;  // global stream for reading from the input file
+
+// Scanner processes only one word each time it is called
+// Gives back the token type and the word itself
+// ** Done by: 
+int scanner(tokentype& tt, string& w)
 {
 
-    // Verb Marker Checks -----
-    if (word == "masu")
-        return "VERB";
+  // ** Grab the next word from the file via fin
+  // 1. If it is eofm, return right now.   
 
-    else if (word == "masen")
-        return "VERBNEG";
+  /*  **
+  2. Call the token functions (word and period) 
+     one after another (if-then-else).
+     Generate a lexical error message if both DFAs failed.
+     Let the tokentype be ERROR in that case.
 
-    else if (word == "mashita")
-        return "VERBPAST";
+  3. If it was a word,
+     check against the reservedwords list.
+     If not reserved, tokentype is WORD1 or WORD2
+     decided based on the last character.
 
-    else if (word == "masendeshita")
-        return "VERBPASTNEG";
+  4. Return the token type & string  (pass by reference)
+  */
 
-    else if (word == "desu")
-        return "IS";
-
-    else if (word == "deshita")
-        return "WAS";
-
-
-    // Particles Checks -----
-    else if (word == "o")
-        return "OBJECT";
-
-    else if (word == "wa")
-        return "SUBJECT";
-
-    else if (word == "ni")
-        return "DESTINATION";
+}//the end of scanner
 
 
-    // Prounouns and connectors check -----
-    else if (word == "watashi" || word == "anata" || word == "kare" || word == "kanojo" || word == "sore")
-        return "PRONOUN";
 
-    else if (word == "mata" || word == "soshite" || word == "shikashi" || word == "dakara")
-        return "CONNECTOR";
+// The temporary test driver to just call the scanner repeatedly  
+// This will go away after this assignment
+// DO NOT CHANGE THIS!!!!!! 
+// Done by:  Louis
+int main()
+{
+  tokentype thetype;
+  string theword; 
+  string filename;
+
+  cout << "Enter the input file name: ";
+  cin >> filename;
+
+  fin.open(filename.c_str());
+
+  // the loop continues until eofm is returned.
+   while (true)
+    {
+       scanner(thetype, theword);  // call the scanner which sets
+                                   // the arguments  
+       if (theword == "eofm") break;  // stop now
+
+       cout << "Type is:" << tokenName[thetype] << endl;
+       cout << "Word is:" << theword << endl;   
+    }
+
+   cout << "End of file is encountered." << endl;
+   fin.close();
+
+}// end
 
 
-    // If none are matching, return none. -----
-    else
-        return "NONE";
-}
+// HELPER FUNCTIONS ---------------------------------
 
 // inputs: a character.
 // outputs: a bool value.
@@ -367,7 +335,6 @@ bool IsVowel(char token)
     // Return true of false if exists in set of vowels.
     return vowels.find(token) != string::npos;
 }
-
 
 // Matthew - I was not necessarily sure what to call these functions.
 // I am not super happy about their names, they are longer than I'd like.
